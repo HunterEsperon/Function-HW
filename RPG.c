b8c1b58875381cdf23b25db5f60df6698833eb03
HunterEsperon <32951100+HunterEsperon@users.noreply.github.com>
2018-08-24 14:06:35 -0500

    Add files via upload

--- RPG.c
+++ RPG.c
@@ -8,42 +8,46 @@
 // Stat Types
 #define _Health 0
 #define _Damage 1
-#define _Heal 2
+#define _HealAmount 2
+#define _NumberOfHeals 3
 //Orders
 #define _Attack 1
-#define _HealthPotion 2
+#define _Heal 2
 #define _Retreat 3
 //bool
 #define _False 0
 #define _True 1
 
 // X = Character Types, Y = Stat Types
-int Characters [2][3] = {{100, 20, 60}, {100, 30, 60}};
+int Characters [2][4] = {{100, 20, 60, 5}, {100, 30, 60, 5}};
 //Did the player retreat
 int PlayerRetreated = _False;
 
-//executes order
+//executes a order
 void ExecuteOrder(int Character, int order);
 //Character Deals Damage to another Character
 void DealDamage(int attackingCharacter, int attackedCharacter);
 //Heal Character
 void Heal(int Character);
 //returns a random order
-int GetOrder();
+int GetOrder(int Character);
 
 int main()
 {
 	while(Characters[_Player][_Health] > 0 && Characters[_Enemy][_Health] && PlayerRetreated == _False)
 	{
-		int playerOrder, enemyOrder = GetOrder();
+		int playerOrder, enemyOrder = GetOrder(_Enemy);
 
+		//prints the plaeyer and enemy health
 		printf("\nPlayers Hp: %d", Characters[_Player][_Health]);
 		printf("\nEnemys Hp: %d", Characters[_Enemy][_Health]);
 
-		puts("\n1. attack \n2. Heal \n3. Retreat");
-		printf("%s", "What do you want to do: ");
+		//asks the plaeyr to chosse a order
+		printf("\n1. attack \n2. Heal | Heals: %d \n3. Retreat", Characters[_Player][_NumberOfHeals]);
+		printf("\n%s", "What do you want to do: ");
 		scanf("%d", &playerOrder);
 
+		//Executes the player and enemy orders
 		ExecuteOrder(_Player, playerOrder);
 		ExecuteOrder(_Enemy, enemyOrder);
 	}
@@ -70,16 +74,31 @@ void ExecuteOrder(int Character, int order)
 				printf("\nEnemy attacked. Dmg: %d", Characters[Character][_Damage]);
 			}
 		break;
-		case _HealthPotion:
-			Heal(Character);
-			if(Character == _Player)
+		case _Heal:
+			if(Characters[Character][_NumberOfHeals] > 0)
 			{
-				printf("\nPlayer healed. Hp: %d", Characters[Character][_Heal]);
+				Heal(Character);
+				if(Character == _Player)
+				{
+					printf("\nPlayer healed. Hp: %d", Characters[Character][_HealAmount]);
+				}
+				else
+				{
+					printf("\nEnemy healed. Hp: %d", Characters[Character][_HealAmount]);
+				}
 			}
 			else
 			{
-				printf("\nEnemy healed. Hp: %d", Characters[Character][_Heal]);
+				if(Character == _Player)
+				{
+					printf("%s %d\n", "No Heals left", Characters[Character][_NumberOfHeals]);
+				}
+				else
+				{
+					printf("%s %d\n", "Error: Enemy heald whith no heals left", Characters[Character][_NumberOfHeals]);
+				}
 			}
+
 		break;
 		case _Retreat:
 		PlayerRetreated = _True;
@@ -99,11 +118,32 @@ void DealDamage(int attackingCharacter, int attackedCharacter)
 
 void Heal(int Character)
 {
-	Characters[Character][_Health] += Characters[Character][_Heal];
+	Characters[Character][_Health] += Characters[Character][_HealAmount];
+	Characters[Character][_NumberOfHeals] -= 1;
 }
 
-int GetOrder()
+int GetOrder(int Character)
 {
 	srand(time(NULL));
-	return rand() % 2 + 1;
+	int order = rand() % 2 + 1;
+
+	switch(order)
+	{
+		case _Attack:
+			return _Attack;
+		break;
+		case _Heal:
+			if(Characters[Character][_NumberOfHeals] > 0)
+			{
+				return _Heal;
+			}
+			else
+			{
+				return _Attack;
+			}
+		break;
+	}
+
+	printf("%s\n", "Error: order not found returning Attack order");
+	return _Attack;
 }
